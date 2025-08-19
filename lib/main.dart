c14d467028a1abe84af2d0a4fdfb4b380551b26f
Eng body <abdalrahmanfekry2@gmail.ocm>
2025-08-19 02:51:20 -0800

    + init commit + initial login screen

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        title: 'Firebase App',
+        home: Scaffold(
+          appBar: AppBar(title: Text('Firebase Connected')),
+          body: Center(child: Text('Success!')),
+        ),
+      ),
+    );
+  }
+}
